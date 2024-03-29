734c4df70cba6ac8d7294dbb5d98043e0a3c72cc
Ruan Pablo Silva <161895419+ruanpablosilva@users.noreply.github.com>
2024-03-29 04:39:56 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Idade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+
+            if (!int.TryParse(txtIdade.Text, out int idade))
+            {
+                MessageBox.Show("Por favor, digite uma idade válida.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (idade >= 18)
+            {
+                MessageBox.Show("Você é maior de idade.", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Você é menor de idade.", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
+// 1  label de instrução e outro nomeando a página, 1 textbox funcional para colocar a idade e um botão de comando
